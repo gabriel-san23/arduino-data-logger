f7c93373b550cbc90166326b5a6928ff37191b99
Oliver Simão Carraro <oliver.s.carraro@gmail.com>
2026-03-21 20:15:46 -0300

    feat: add sub-menu system for individual sensor data display

--- arduino_data_logger/arduino_data_logger.ino
+++ arduino_data_logger/arduino_data_logger.ino
@@ -1,106 +1,139 @@
-#include <LiquidCrystal_I2C.h> 
+#include <LiquidCrystal_I2C.h>
 #include "DHT.h"
-
-int botao_ok = 3; 
-int botao_direita = 4; 
-int botao_esquerda = 5; 
+ 
+int botao_ok = 3;
+int botao_direita = 4;
+int botao_esquerda = 5;
 int botao_menu = 6;
-int escolha = 0; 
-
+ 
+bool modoFahrenheit = false;
+ 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-
-// Configurações do DHT22 (lembre que no laboratório tempos o DHT-11)
+ 
+// Configurações do DHT22 (lembre que no laboratório temos o DHT-11)
 #define DHTPIN 2
 #define DHTTYPE DHT22 //Mudar para DHT-11 no laboratório
 DHT dht(DHTPIN, DHTTYPE);
-
-void setup() 
-{ 
-  pinMode(botao_ok, INPUT); 
-  pinMode(botao_direita, INPUT); 
-  pinMode(botao_esquerda, INPUT); 
-  pinMode(botao_menu, INPUT);
-  
-  lcd.begin(16, 2); 
-  dht.begin();
-  
-  lcd.clear();  
-  atualizaVisor(); 
-} 
-
-void loop() 
-{ 
-    
-  do  
-  { 
-    if (digitalRead(botao_direita) == HIGH) 
-    { 
-      escolha++; 
-      // Trava no limite máximo de opções (2)
-      if (escolha > 2) {
-        escolha = 2; 
-      }
-      atualizaVisor();     
-      delay(250); 
-    } 
-
-    if (digitalRead(botao_esquerda) == HIGH) 
-    { 
-      escolha--; 
-      // Trava no limite mínimo (0)
-      if (escolha < 0) {
-        escolha = 0; 
-      }
-      atualizaVisor(); 
-      delay(250); 
-    } 
-    
-    if (digitalRead(botao_menu) == HIGH)   
-    {
-      escolha = 0; 
-      atualizaVisor(); 
-      delay(250); 
-    }
-  } while (digitalRead(botao_ok) != HIGH); 
-
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("Selecionado!"); 
-  delay(3000); 
-  atualizaVisor(); 
-} 
-
-void atualizaVisor()
+ 
+// Configurações do LDR
+#define LDRPIN A3
+ 
+void setup()
 {
-  lcd.clear(); 
-  lcd.setCursor(0,0); 
-  
-  if (escolha == 0)
+  pinMode(LDRPIN, INPUT);
+  pinMode(botao_ok, INPUT);
+  pinMode(botao_direita, INPUT);
+  pinMode(botao_esquerda, INPUT);
+  pinMode(botao_menu, INPUT);
+ 
+  lcd.begin(16, 2);
+  dht.begin();
+ 
+  lcd.clear();  
+}
+ 
+void loop()
+{
+  telaDados();
+  telaEscolha();
+}
+ 
+void telaDados()
+{
+  int subTela = 0;
+  bool sair = false;
+ 
+  lcd.clear();
+ 
+  while (!sair)
   {
-    // Ler os valores de temperatura e umidade
-    float humidity = dht.readHumidity();
-    float temperature = dht.readTemperature();
-    lcd.setCursor(0,0);
-    lcd.print("H: ");
-    lcd.setCursor(4,0);
-    lcd.print(humidity);
-    lcd.setCursor(0,1);
-    lcd.print("T: ");
-    lcd.setCursor(4,1);
-    lcd.print(temperature);
-   
+    if (subTela == 0)
+    {
+      float temperature = dht.readTemperature();
+      if (modoFahrenheit) {
+        temperature = (1.8 * temperature) + 32.0;
+      }
+ 
+      lcd.setCursor(0,0);
+      lcd.print("T: ");
+      lcd.print(temperature);
+     
+      if (modoFahrenheit) lcd.print(" F  ");
+      else lcd.print(" C  ");
+    }
+    else if (subTela == 1)
+    {
+      float humidity = dht.readHumidity();
+      lcd.setCursor(0,0);
+      lcd.print("H: ");
+      lcd.print(humidity);
+      lcd.print(" %  ");
+    }
+    else if (subTela == 2)
+    {      
+      int lighting = analogRead(LDRPIN);
+      lcd.setCursor(0,0);
+      lcd.print("*L: ");
+      lcd.print(lighting);
+      lcd.print("    ");
+    }
+ 
+    if (digitalRead(botao_ok) == HIGH)
+    {
+      subTela++;
+      if (subTela > 2) {
+        subTela = 0;
+      }
+      lcd.clear();
+     
+      while(digitalRead(botao_ok) == HIGH) {
+        delay(10);
+      }
+      delay(50);
+    }
+ 
+    if (digitalRead(botao_direita) == HIGH || digitalRead(botao_menu) == HIGH)
+    {
+      sair = true;
+     
+      while(digitalRead(botao_direita) == HIGH || digitalRead(botao_menu) == HIGH) {
+        delay(10);
+      }
+      delay(50);
+    }
+    delay(100);
   }
-  else if (escolha == 1) 
-  {
-    lcd.print("Fahrenheit F"); 
-    lcd.setCursor(0,1); 
-    lcd.print("     <= OK =>  ");
-  }  
-
-  else if (escolha == 2) 
-  {
-    lcd.print("Celsius C"); 
-    lcd.setCursor(0,1); 
-    lcd.print("     <= OK     ");
-  }  
+}
+ 
+void telaEscolha()
+{
+  bool sair = false;
+  lcd.clear();
+ 
+  while(!sair) {
+    lcd.setCursor(0,0);
+    lcd.print("Unidade Temp:");
+   
+    lcd.setCursor(0,1);
+    if (modoFahrenheit) lcd.print("< Fahrenheit >  ");
+    else lcd.print("< Celsius    >  ");
+ 
+    if (digitalRead(botao_esquerda) == HIGH || digitalRead(botao_direita) == HIGH) {
+      modoFahrenheit = !modoFahrenheit;
+     
+      while(digitalRead(botao_esquerda) == HIGH || digitalRead(botao_direita) == HIGH) {
+        delay(10);
+      }
+      delay(50);
+    }
+ 
+    if (digitalRead(botao_ok) == HIGH || digitalRead(botao_menu) == HIGH) {
+      sair = true;
+     
+      while(digitalRead(botao_ok) == HIGH || digitalRead(botao_menu) == HIGH) {
+        delay(10);
+      }
+      delay(50);
+    }
+  }
 }
